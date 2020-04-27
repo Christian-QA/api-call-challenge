0c8ae70fee302045f442c524caa6d32c506b5220
Korbinian <kmr.ring@outlook.com>
2020-04-27 13:56:07 +0100

    added delte user function

--- User/deleteUser.js
+++ User/deleteUser.js
@@ -1,21 +1,20 @@
-let deleteData = '{"username" : "Korbinian"}'
 
 function deleteUser() {
-    let obj = JSON.parse(deleteData);
-    REQ.open('DELETE', `http://petstore.swagger.io/v2/user/${obj.username}`);
+    REQ.open('DELETE', `http://petstore.swagger.io/v2/user/${sessionStorage.getItem("name")}`);
     REQ.setRequestHeader('Content-Type', 'Application/json');
     // REQ.setRequestHeader('Access-Control-Allow-Origin', '*'); leave this out, it throws error
     REQ.onload = () => {
         if (REQ.status === 200) {
             console.log(REQ.response);
+            getLogOut();
         } else {
             console.log('handle error');
         }
     }
-    REQ.send(deleteData); // Waht we want to send across
+    REQ.send(); // Waht we want to send across
 }
 
-let buttDeleteUser = document.querySelector('#buttDeleteUser');
+let buttDeleteUser = document.querySelector('#deleteUser');
 buttDeleteUser.addEventListener('click', deleteUser);
 
 
